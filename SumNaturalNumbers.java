6b0c8daade2ab3b6ef1b37d6e675938fd898850d
ankush1038 <ankusharma1029@gmail.com>
2025-03-01 14:17:36 +0530

    [Ankush] ADD: Sum of natural numbers calculated

---
+++ SumNaturalNumbers.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter a natural number: ");
+        int n = sc.nextInt();
+
+        // Checking if the number is a natural number
+        if (n >= 1) {
+            // Computing sum using formula
+            int sumFormula = n * (n + 1) / 2;
+
+            // Computing sum using while loop
+            int sumLoop = 0, i = 1;
+            while (i <= n) {
+                sumLoop += i;
+                i++;
+            }
+
+            // Displaying results
+            System.out.println("Sum using formula: " + sumFormula);
+            System.out.println("Sum using while loop: " + sumLoop);
+
+            // Comparing the results
+            if (sumFormula == sumLoop) {
+                System.out.println("Both results are correct.");
+            } else {
+                System.out.println("There is an error in computation.");
+            }
+        } else {
+            System.out.println("The number " + n + " is not a natural number.");
+        }
+
+        sc.close();
+    }
+}
