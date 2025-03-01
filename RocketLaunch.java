e85e75c90400f3a17cfe8fb5de7ef914e97d0d56
ankush1038 <ankusharma1029@gmail.com>
2025-03-01 14:09:20 +0530

    [Ankush] ADD: Measured rocket lauch count

---
+++ RocketLaunch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RocketLaunch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking countdown start value from the user
+        System.out.print("Enter the countdown start number: ");
+        int counter = sc.nextInt();
+
+        // Countdown using for loop
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Displaying launch message
+        System.out.println("Lift-off");
+
+        sc.close();
+    }
+}
