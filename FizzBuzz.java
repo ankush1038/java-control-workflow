fdbba90b9fba67fb99f28efd77482da660500a29
ankush1038 <ankusharma1029@gmail.com>
2025-03-01 15:18:10 +0530

    [Ankush] ADD: FizzBUzz printed

---
+++ FizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        // Checking if the number is a positive integer
+        if (number > 0) {
+            // Loop from 1 to the entered number
+            for (int i = 1; i <= number; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("The number " + number + " is not a positive integer.");
+        }
+
+        sc.close();
+    }
+}
