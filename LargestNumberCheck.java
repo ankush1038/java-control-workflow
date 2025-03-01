bb759fe7b52365da84f360b1d000fbdc5fba52ae
ankush1038 <ankusharma1029@gmail.com>
2025-03-01 13:47:23 +0530

    [Ankush] ADD: Found largest number

---
+++ LargestNumberCheck.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LargestNumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from the user
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+        
+        System.out.print("Enter third number: ");
+        int number3 = sc.nextInt();
+
+        // Checking if the first number is the largest
+        if (number1 > number2 && number1 > number3) {
+            System.out.println("Is the first number the largest? Yes");
+        } else {
+            System.out.println("Is the first number the largest? No");
+        }
+
+        // Checking if the second number is the largest
+        if (number2 > number1 && number2 > number3) {
+            System.out.println("Is the second number the largest? Yes");
+        } else {
+            System.out.println("Is the second number the largest? No");
+        }
+
+        // Checking if the third number is the largest
+        if (number3 > number1 && number3 > number2) {
+            System.out.println("Is the third number the largest? Yes");
+        } else {
+            System.out.println("Is the third number the largest? No");
+        }
+
+        sc.close();
+    }
+}
